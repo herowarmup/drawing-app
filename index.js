792b10c991a502427129a23a8ceeb3e666be17e3
Artem Morozov <deadtems@yandex.ru>
2022-12-19 04:55:19 +0500

    fead: add rectangle

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const canvas = document.querySelector('canvas');
+toolBtns = document.querySelectorAll('.tool');
 ctx = canvas.getContext('2d');
 
-const drawing = (e) => {
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+let prevMouseX,
+  prevMouseY,
+  snapshot,
+  isDrawing = false,
+  selectedTool = 'brush',
+  brushWidth = 5;
+
+window.addEventListener('load', () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+});
+
+const drawRect = (e) => {
+  ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
 };
 
+const startDraw = (e) => {
+  isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
+  ctx.beginPath();
+  ctx.lineWidth = brushWidth;
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+const drawing = (e) => {
+  if (!isDrawing) return;
+  ctx.putImageData(snapshot, 0, 0);
+
+  if (selectedTool === 'brush') {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  } else if (selectedTool === 'rectangle') {
+    drawRect(e);
+  }
+};
+
+toolBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.options .active').classList.remove('active');
+    btn.classList.add('active');
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
+
+canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', drawing);
+canvas.addEventListener('mouseup', () => (isDrawing = false));
